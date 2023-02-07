19828989ff6c0cdd08e869de143d3ff33c231b58
Otabekismailov <15ismailov1995@gmail.com>
2023-02-07 23:07:40 +0500

    Task 6 was done

--- task-3model/task_6/task_6_json_.py
+++ task-3model/task_6/task_6_json_.py
@@ -1,21 +1,22 @@
-import json
 import requests
-from tkinter import Tk, Frame, Text, Label, END, Button
-import textblob
-from tkinter import ttk, messagebox
 
 kurs = {}
 
 
 def get_exchange_rate():
-    url = f'https://api.freecurrencyapi.com/v1/latest?apikey=XXdZcMz6fXSDKcG5wOBydY804DH0mx64Amh2Koqu'
+    url = 'https://v6.exchangerate-api.com/v6/11703d1b314f811793cbdfd9/latest/USD'
     response = requests.get(url)
-    res = json.loads(response.text)
-    kurs.update(res['data'])
-    print(kurs)
+    res = response.json()
+    res = dict(res)
+    kurs.update(res['conversion_rates'])
 
 
 get_exchange_rate()
+
+from tkinter import *
+import textblob
+from tkinter import ttk, messagebox
+
 kurs_conver = Tk()
 kurs_conver.title("P10_Kurs -> Conver")
 
@@ -40,6 +41,7 @@ def conver_text():
     resulta2 = ""
     try:
         trans_text.delete(1.0, END)
+        # if original_combo in "".isalpha() and trans_combo in "".isalpha():
         for key, valu in conver_dik.items():
             if key == original_combo.get():
                 resulta1 += key
@@ -48,13 +50,17 @@ def conver_text():
             if key == trans_combo.get():
                 res2 = valu
                 resulta2 += key
+
         words = textblob.TextBlob(original_text.get(1.0, END))
         words = str(words)
-        res3 = float(res2) / float(res1) * int(words)
+        res3 = float(words) * float(res2)
         trans_text.insert(1.0, str(res3), END)
 
         messagebox.showinfo("Converter!", f"Tanlangan Valutani,Nomi {resulta1} Miqdori --> {words}"
                                           f"Tanlangan Valuta ,Nomi  {resulta2}  Miqdori --> {res3}")
+    # else:
+    #     messagebox.showerror("Erorr", "\nIltimos Valutani"
+    #                                   "\nKiriting!")
 
     except:
         messagebox.showerror("Erorr", "\nXatolik Yuz Berdi"
@@ -67,19 +73,18 @@ def clear():
 
 
 conver_dik = kurs
-list_convert1 = list(kurs.keys())
+conver_list = list(kurs.keys())
 
-button_trans = Button(kurs_conver, text="<<<Converter>>>!", width=20, bg="red", font=30, command=conver_text)
+button_trans = Button(kurs_conver, text="<<<Converter>>>!", width=20, bg="red", font=30, command=conver_text, )
 button_trans.place(x=280, y=150)
 
-original_combo = ttk.Combobox(kurs_conver, width=20, values=list_convert1)
-
+original_combo = ttk.Combobox(kurs_conver, width=20, values=conver_list)
 original_combo.place(x=0, y=350)
 
 trans_label1 = Label(kurs_conver, text="Qaysi_Valutaga,Tanlang", bg="red", font=20, )
 trans_label1.place(x=600, y=320)
 
-trans_combo = ttk.Combobox(kurs_conver, width=20, values=list_convert1)
+trans_combo = ttk.Combobox(kurs_conver, width=20, values=conver_list)
 trans_combo.place(x=620, y=350)
 
 orta_label = Label(kurs_conver, text="------>", bg="yellow", font=100)
@@ -88,5 +93,5 @@ orta_label.place(x=280, y=350)
 cler_button = Button(kurs_conver, text="Clear", bg="red", font=60, command=clear)
 cler_button.place(x=350, y=350)
 
-if __name__ == "__main__":
+if __name__ == ("__main__"):
     kurs_conver.mainloop()
---
+++ task-3model/task_6/task_6_weather.py
@@ -0,0 +1,141 @@
+import csv
+from datetime import datetime
+import json
+import json
+import time
+import tkinter
+from tkinter import Label, Entry, Button, Text, END, ttk
+import requests
+
+
+class WeatherManager:
+    # API_KEY = 'ZQ48KUlOHW4H7bnsvgNlF02Z06kwKvoB'
+
+    # API_KEY = 'hXhEWejPhlsDzrFaVzeJd2GvTQcIagIE'
+
+    API_KEY = 'AFKoBhI9HsbH6BnLsddIqu8xAoT1T5RC'
+
+    def __init__(self, city):
+        self.city = city
+
+    @staticmethod
+    def convert_to_datetime(date_str):
+        return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")
+
+    def get_data(self):
+        url = f"https://api.tomorrow.io/v4/weather/forecast?" \
+              f"location={self.city}&apikey={self.API_KEY}"
+        headers = {"accept": "application/json"}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return json.loads(response.text)
+        else:
+            return json.loads(response.text)
+
+    def get_timelines(self):
+        res = self.get_data()
+        return res.get("timelines")
+
+    def get_daily_data(self):
+        timelines = self.get_timelines()
+        if timelines:
+            return timelines.get("daily")
+
+    def get_hourly_data(self):
+        timelines = self.get_timelines()
+        if timelines:
+            return timelines.get("hourly")
+
+    def get_day_hours_temperature_with_time(self, day_date):
+        hourly_data = self.get_hourly_data()
+        data = []
+
+        for hour_data in hourly_data:
+            time = hour_data.get("time")
+            if self.convert_to_datetime(time).date() == day_date.date():
+                data.append({
+                    "time": self.convert_to_datetime(time).strftime("%H:%M"),
+                    "temperature": hour_data["values"].get("temperature")
+                })
+        return data
+
+    def get_daily_temperature(self):
+        data = []
+
+        for day in self.get_daily_data():
+            day_values = day.get("values")
+            average_temperature = None
+            if day_values:
+                average_temperature = day_values.get("temperatureAvg")
+            day_date = datetime.strptime(day.get("time"), "%Y-%m-%dT%H:%M:%SZ")
+            data.append({
+                "day": day_date.strftime("%Y.%m.%d"),
+                "average_temperature": average_temperature,
+                "hours": self.get_day_hours_temperature_with_time(day_date)}
+            )
+
+        return data
+
+
+# # print(WeatherManager('tashkent').get_daily_temperature())
+#
+#
+def time2():
+    weather_data.configure(text=WeatherManager(weather_entery.get()).get_daily_temperature()[0]['day'])
+    weather_temperatureAvg.configure(
+        text=WeatherManager(weather_entery.get()).get_daily_temperature()[0]['average_temperature'])
+    for i in WeatherManager(weather_entery.get()).get_daily_temperature()[0]['hours']:
+        if i.get('time') <= datetime.now().strftime("%H:%M"):
+            weather_temp2.configure(text=i.get('temperature'))
+            weather_time2.configure(text=i.get('time'))
+        if i.get('time') >= datetime.now().strftime("%H:%M"):
+            weather_temp3.configure(text=i.get('temperature'))
+            weather_time3.configure(text=i.get('time'))
+
+
+weather = tkinter.Tk()
+weather.title("P10_Weather")
+
+# weather.resizable(width=False, height=False)
+weather_color = tkinter.Frame(weather, height=400, width=800, )
+weather_color.grid(row=0, column=1)
+
+weter_heder = Label(weather, text="Weather Applecition", font=50)
+weter_heder.place(x=300, y=0)
+
+weather_label = Label(weather, text="Select City:", bg='red', font=50)
+weather_label.place(x=200, y=50)
+
+weather_data = Label(weather, text="", bg="red", font=20)
+weather_data.place(x=100, y=90)
+
+weather_tempertura_label = Label(weather, text="O'rtacha harorat:", font=40)
+weather_tempertura_label.place(x=250, y=90)
+
+weather_temperatureAvg = Label(weather, text="", bg="red", font=20)
+weather_temperatureAvg.place(x=400, y=90)
+
+weather_time = Label(weather, text="VAQT:", bg="red")
+weather_time.place(x=100, y=120)
+
+weather_time2 = Label(weather, text="", bg='red')
+weather_time2.place(x=160, y=120)
+weather_time3 = Label(weather, text="", bg='red')
+weather_time3.place(x=220, y=120)
+
+weather_temp = Label(weather, text="Harorat:", bg="red")
+weather_temp.place(x=100, y=150)
+
+weather_temp2 = Label(weather, text="", bg="red")
+weather_temp2.place(x=160, y=150)
+
+weather_temp3 = Label(weather, text="", bg='red')
+weather_temp3.place(x=220, y=150)
+weather_entery = Entry(weather, width=20, font=20)
+weather_entery.place(x=300, y=50)
+
+weather_button = Button(weather, text="Enter", bg="red", font=60, command=time2)
+weather_button.place(x=510, y=50)
+
+if __name__ == "__main__":
+    weather.mainloop()
